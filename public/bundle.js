00f2e3f5919bb77edefaa54436245580952cba60
Hamza El Ouakili <hamzaelouakili@Hamzas-MacBook-Pro.local>
2017-07-26 15:46:21 +0200

    9_ Use action creators

--- public/bundle.js
+++ public/bundle.js
@@ -1,12 +1,27 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-//
-//	{
-// 		cards: [],
-// 		decks: []
-// 	}
-//
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+//Add_Deck
+//SHOW_ADD_DECK
+//HIDE_ADD_DECK
+
+var _addDeck = function _addDeck(name) {
+  return { type: 'ADD_DECK', data: name };
+};
+var _showAddDeck = function _showAddDeck() {
+  return { type: 'SHOW_ADD_DECK' };
+};
+var _hideAddDeck = function _hideAddDeck() {
+  return { type: 'HIDE_ADD_DECK' };
+};
 
 var cards = function cards(state, action) {
   switch (action.type) {
@@ -21,50 +36,131 @@ var cards = function cards(state, action) {
   }
 };
 
+var decks = function decks(state, action) {
+  switch (action.type) {
+    case 'ADD_DECK':
+      var newDeck = { name: action.data, id: +new Date() };
+      return state.concat([newDeck]);
+    default:
+      return state || [];
+  }
+};
+
+var addingDeck = function addingDeck(state, action) {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK':
+      return true;
+    case 'HIDE_ADD_DECK':
+      return false;
+    default:
+      return !!state;
+  }
+};
+
 var store = Redux.createStore(Redux.combineReducers({
-  cards: cards // cards -> short hand notation of (es6) -> cards(property name): cards(value is reference to reducer function) or cards: cards(state.cards, action)
+  cards: cards,
+  decks: decks,
+  addingDeck: addingDeck
 }));
 
 var App = function App(props) {
   return React.createElement(
-    "div",
-    { className: "app" },
+    'div',
+    { className: 'app' },
     props.children
   );
 };
 
-var Sidebar = React.createClass({
-  displayName: "Sidebar",
-  render: function render() {
-    var props = this.props;
-    return React.createElement(
-      "div",
-      { className: "sidebar" },
-      React.createElement(
-        "h2",
-        null,
-        "All Decks"
-      ),
-      React.createElement(
-        "ul",
-        null,
-        props.decks.map(function (deck, i) {
-          return React.createElement(
-            "li",
-            { key: i },
-            deck.name
-          );
-        })
-      ),
-      props.addingDeck && React.createElement("input", { ref: "add" })
-    );
-  }
-});
+var Sidebar = function (_React$Component) {
+  _inherits(Sidebar, _React$Component);
 
-ReactDOM.render(React.createElement(
-  App,
-  null,
-  React.createElement(Sidebar, { decks: [{ name: 'Deck 1' }], addingDeck: false })
-), document.getElementById('root'));
+  function Sidebar(props) {
+    _classCallCheck(this, Sidebar);
+
+    var _this = _possibleConstructorReturn(this, (Sidebar.__proto__ || Object.getPrototypeOf(Sidebar)).call(this, props));
+
+    _this.createDeck = _this.createDeck.bind(_this);
+    return _this;
+  }
+
+  _createClass(Sidebar, [{
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate() {
+      var el = ReactDOM.findDOMNode(this.refs.add);
+      if (el) el.focus();
+    }
+  }, {
+    key: 'createDeck',
+    value: function createDeck(e) {
+      if (e.which !== 13) return;
+
+      var name = ReactDOM.findDOMNode(this.refs.add).value;
+      this.props.addDeck(name);
+      this.props.hideAddDeck();
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var props = this.props;
+      return React.createElement(
+        'div',
+        { className: 'sidebar' },
+        React.createElement(
+          'h2',
+          null,
+          'All Decks'
+        ),
+        React.createElement(
+          'button',
+          { onClick: function onClick(e) {
+              return _this2.props.showAddDeck();
+            } },
+          'New Deck'
+        ),
+        React.createElement(
+          'ul',
+          null,
+          props.decks.map(function (deck, i) {
+            return React.createElement(
+              'li',
+              { key: i },
+              deck.name
+            );
+          })
+        ),
+        props.addingDeck && React.createElement('input', { ref: 'add', onKeyPress: this.createDeck })
+      );
+    }
+  }]);
+
+  return Sidebar;
+}(React.Component);
+
+function run() {
+  var state = store.getState();
+  console.log(state);
+  ReactDOM.render(React.createElement(
+    App,
+    null,
+    React.createElement(Sidebar, {
+      decks: state.decks,
+      addingDeck: state.addingDeck,
+      addDeck: function addDeck(name) {
+        return store.dispatch(_addDeck(name));
+      },
+      showAddDeck: function showAddDeck() {
+        return store.dispatch(_showAddDeck());
+      },
+      hideAddDeck: function hideAddDeck() {
+        return store.dispatch(_hideAddDeck());
+      }
+    })
+  ), document.getElementById('root'));
+}
+
+run();
+store.subscribe(run);
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-//
-//	{
-// 		cards: [],
-// 		decks: []
-// 	}
-//
+//Add_Deck
+//SHOW_ADD_DECK
+//HIDE_ADD_DECK
+
+const addDeck = name => ({ type: 'ADD_DECK', data: name })
+const showAddDeck = () => ({ type: 'SHOW_ADD_DECK' })
+const hideAddDeck = () => ({ type: 'HIDE_ADD_DECK' })
 
 const cards = (state, action) => {
   switch (action.type) {
@@ -18,9 +19,32 @@ const cards = (state, action) => {
   }
 }
 
+const decks = (state, action) => {
+  switch (action.type) {
+    case 'ADD_DECK':
+      let newDeck = { name: action.data, id: +new Date() }
+      return state.concat([newDeck])
+    default:
+      return state || []
+  }
+}
+
+const addingDeck = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ADD_DECK':
+      return true
+    case 'HIDE_ADD_DECK':
+      return false
+    default:
+      return !!state
+  }
+}
+
 const store = Redux.createStore(
   Redux.combineReducers({
-    cards // cards -> short hand notation of (es6) -> cards(property name): cards(value is reference to reducer function) or cards: cards(state.cards, action)
+    cards,
+    decks,
+    addingDeck
   })
 )
 
@@ -32,12 +56,31 @@ const App = props => {
   )
 }
 
-const Sidebar = React.createClass({
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.createDeck = this.createDeck.bind(this)
+  }
+
+  componentDidUpdate() {
+    let el = ReactDOM.findDOMNode(this.refs.add)
+    if (el) el.focus()
+  }
+
+  createDeck(e) {
+    if (e.which !== 13) return
+
+    let name = ReactDOM.findDOMNode(this.refs.add).value
+    this.props.addDeck(name)
+    this.props.hideAddDeck()
+  }
+
   render() {
     let props = this.props
     return (
       <div className="sidebar">
         <h2>All Decks</h2>
+        <button onClick={e => this.props.showAddDeck()}>New Deck</button>
         <ul>
           {props.decks.map((deck, i) =>
             <li key={i}>
@@ -45,15 +88,28 @@ const Sidebar = React.createClass({
             </li>
           )}
         </ul>
-        {props.addingDeck && <input ref="add" />}
+        {props.addingDeck && <input ref="add" onKeyPress={this.createDeck} />}
       </div>
     )
   }
-})
+}
 
-ReactDOM.render(
-  <App>
-    <Sidebar decks={[{ name: 'Deck 1' }]} addingDeck={false} />
-  </App>,
-  document.getElementById('root')
-)
+function run() {
+  let state = store.getState()
+  console.log(state)
+  ReactDOM.render(
+    <App>
+      <Sidebar
+        decks={state.decks}
+        addingDeck={state.addingDeck}
+        addDeck={name => store.dispatch(addDeck(name))}
+        showAddDeck={() => store.dispatch(showAddDeck())}
+        hideAddDeck={() => store.dispatch(hideAddDeck())}
+      />
+    </App>,
+    document.getElementById('root')
+  )
+}
+
+run()
+store.subscribe(run)
