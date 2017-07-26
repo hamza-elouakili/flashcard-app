f98d2a184c49a0bdb9a168effb38e947b2daccdc
Hamza El Ouakili <hamzaelouakili@Hamzas-MacBook-Pro.local>
2017-07-26 11:47:22 +0200

    4 - reducers and actions

--- public/bundle.js
+++ public/bundle.js
@@ -1,6 +1,46 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-console.log('Hello react');
+//1_You create a store by calling the Redux.createStore
+//2_You pass in a callback function which contains reducers which will modify the state according to a given action
+//3_You create a switch statement which will determine based on which action type it receives which reducer to execute on the state
+//4_All changes to the state happens through the store
+//5_The return value is always a new state, which is then managed by the state and whenever you call dispatch the previous state is then consulted
+var store = Redux.createStore(function (state, action) {
+  switch (action.type) {
+    case 'ADD_CARD':
+      var newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date()
+      });
+      return Object.assign({}, state, {
+        cards: state.cards ? state.cards.concat([newCard]) : [newCard]
+      });
+    default:
+      return state || {};
+  }
+});
+
+//Whenever there is change made to the state managed by the state, all the functions which subscribe to store will be executed
+store.subscribe(function () {
+  console.log(store.getState());
+});
+
+//We want to change the state by calling the dispatch method on the store and passing it
+//an action type of what to do, and data to modify the state with how to modify the state
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {
+    front: 'front',
+    back: 'back'
+  }
+});
+
+//We want to change the state once more, by calling the dispatch function
+//with an action type and data to modify the state with
+store.dispatch({
+  type: 'ADD_CARD',
+  data: {}
+});
 
 },{}]},{},[1]);
